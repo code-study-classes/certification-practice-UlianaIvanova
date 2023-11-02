75abc1098c2652e3a631f224d3929bcf60ab0ad2
Uliana <uli.a.na.iva.nova@mail.ru>
2023-11-02 15:57:49 +0300

    1

---
+++ Numbers.js
@@ -0,0 +1,7 @@
+export function calculateDistance(x1, x2) {
+  return Math.abs(x2 - x1);
+}
+
+const x1 = 3;
+const x2 = 10;
+console.log(calculateDistance(x1, x2));
--- numbers.js
+++ numbers.js
@@ -1,1 +1,16 @@
-
+function calculateDistance(x1, x2) {
+  return Math.abs(x2 - x1);
+}
+console.log(calculateDistance(0, 5));
+function calculateSegmentProduct() {}
+function calculateKilobytes() {}
+function calculateSegments() {}
+function calculateDigitSum() {}
+function swapHundredsAndTens() {}
+function getHundredsDigit() {}
+function getFullHours() {}
+function getDayOfWeek() {}
+function countSquares() {}
+export {
+  calculateDistance, calculateSegmentProduct, calculateKilobytes, calculateSegments, calculateDigitSum, swapHundredsAndTens, getHundredsDigit, getFullHours, getDayOfWeek, countSquares,
+};
